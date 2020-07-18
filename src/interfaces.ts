e9d53652f8002f667e46ad497d1179dde30dd3e7
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-07-18 14:42:33 +0300

    add interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,13 @@
+import { PLACES_IDS } from './constants';
+export interface IPlace {
+  id: string;
+  name: string;
+  parts: string[] | undefined;
+}
+
+export interface IInventory {
+  id?: string;
+  name: string;
+  count: number;
+  placeId: string;
+}
