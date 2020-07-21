efe26389e02891241c1dbe812abde61816c85842
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-07-21 11:15:19 +0300

    Updates

---
+++ precache-manifest.8c2fc5cc3eb5fdd0e7316c0165d09f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c8adcad3ad40b969c1e26ef1572acb",
+    "url": "/docsvision-test-task/index.html"
+  },
+  {
+    "revision": "20e9401e1b20c2838643",
+    "url": "/docsvision-test-task/static/css/main.a74799e5.chunk.css"
+  },
+  {
+    "revision": "31fcdc3baa6fac0fad95",
+    "url": "/docsvision-test-task/static/js/2.d96868d7.chunk.js"
+  },
+  {
+    "revision": "c41e6402fe42d52c53ef48b96ca296e6",
+    "url": "/docsvision-test-task/static/js/2.d96868d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e9401e1b20c2838643",
+    "url": "/docsvision-test-task/static/js/main.8f18a1f5.chunk.js"
+  },
+  {
+    "revision": "c2b21146625c0020c616",
+    "url": "/docsvision-test-task/static/js/runtime-main.751464f2.js"
+  }
+]);
